7a4dee5c064cc3e1f56ac59308aa3dce9db688e8
mahesh <mail.mahesh.lk@gmail.com>
2021-07-21 18:30:28 +0800

    go-bigquery-client

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mahesh-dilhan/google-bigquery
 
 go 1.16
+
+require (
+	cloud.google.com/go/bigquery v1.19.0 // indirect
+	google.golang.org/api v0.50.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
+)
+
+// browseTable demonstrates reading data from a BigQuery table directly without the use of a query.
+// For large tables, we also recommend the BigQuery Storage API.
+func browseTable(w io.Writer, projectID, datasetID, tableID string) error {
+	// projectID := "my-project-id"
+	// datasetID := "mydataset"
+	// tableID := "mytable"
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	table := client.Dataset(datasetID).Table(tableID)
+	it := table.Read(ctx)
+	for {
+		var row []bigquery.Value
+		err := it.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, row)
+	}
+	return nil
+}
